ffc78f46657c8efb263926277cac43b609d63161
Francisco Netto <franciscofesilva16@gmail.com>
2019-03-17 14:40:46 -0300

    Commit inicial.
    Adicionado projeto básico gerado pelo assistente de criação do Spring Boot.

---
+++ src/test/java/com/ffsilva/api/DesafioTegraApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ffsilva.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesafioTegraApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
